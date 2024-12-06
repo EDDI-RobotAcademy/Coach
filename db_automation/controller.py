d04a74f746f66f21a82b847089927ff5c2bc584a
ajeseung <ajeseung@naver.com>
2024-12-06 14:54:18 +0900

    fix:[손승일]잘못 올린 파일들 삭제

--- db_automation/controller.py
+++
@@ -1,6 +0,0 @@
-from django.http import JsonResponse
-import json
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'index.html')
--- db_automation/order/serializer/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from fruit_mart.order.entity.order import Order
-
-
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = ['id','customer' ,'number']
